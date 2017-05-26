0f8f7f407ef62a09ea845f41d6eae3b5b88f0435
lukas.steiger@siemens.com <lukas.steiger@hotmail.com>
2017-05-26 10:11:06 +0200

    fix NPE in getDeepKey utility

--- src/components/jazz-helpers/Utils.js
+++ src/components/jazz-helpers/Utils.js
@@ -11,6 +11,8 @@ export default class Utils {
       const keys = keyPath.split('.');
       for (var i = 0; i < keys.length; i++) {
          obj = obj[keys[i]];
+         if(obj === null || typeof obj === "undefined")
+            return null;
       };
       return obj;
    }
--- src/components/jazz-helpers/Utils.test.js
+++ src/components/jazz-helpers/Utils.test.js
@@ -1,17 +1,28 @@
 import Utils from './Utils';
 
 describe("Utils", function () {
-   it("exprect getDeepKey to work", function () {
-      const obj = {
-         "net": {
-            "jazz": {
-               "ajax": {
-                  "_contextRoot": "ccm"
-               }
+   const obj = {
+      "net": {
+         "jazz": {
+            "ajax": {
+               "_contextRoot": "ccm"
             }
          }
-      };
+      }
+   };
+
+   it("exprect getDeepKey to work", function () {
       const res = Utils.getDeepKey("net.jazz.ajax._contextRoot", obj);
       expect(res).toBe("ccm");
+   });
+
+   it("to find no value for wrong key", function () {
+      const res = Utils.getDeepKey("net.foo", obj);
+      expect(res).toBe(null);
+   });
+
+   it("to find no value for wrong deep key", function () {
+      const res = Utils.getDeepKey("net.foo.bar", obj);
+      expect(res).toBe(null);
    });
 });
